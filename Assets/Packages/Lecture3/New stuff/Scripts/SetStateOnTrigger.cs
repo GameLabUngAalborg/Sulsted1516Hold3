fff9af2b311d6d4f5fb1c6338ca52a7ed704a4e0
Christian Skriver Kragegaard <mail@code.dk>
2016-02-17 10:08:07 +0100

    første

---
+++ Assets/Packages/Lecture3/New stuff/Scripts/SetStateOnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStateOnTrigger : MonoBehaviour
+{
+    public enum SetState {  EnableObject, DisableObject }
+
+    public string TagThatTriggersMe = "Player";
+    public SetState SetObject;
+    public GameObject ObjectToSet;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == TagThatTriggersMe)
+        {
+            if(SetObject == SetState.EnableObject)
+            {
+                ObjectToSet.SetActive(true);
+            }
+            else
+            {
+                ObjectToSet.SetActive(false);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
