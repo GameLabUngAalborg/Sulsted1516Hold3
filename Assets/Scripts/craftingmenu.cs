16830d47ea13cd5b0e05623847eab93a23bd2abe
GameLabUngAalborg <git@game2grow.dk>
2016-04-06 15:02:43 +0200

    Added files via upload

---
+++ Assets/Scripts/craftingmenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class craftingmenu : MonoBehaviour {
+	public GameObject Panel;
+	private bool open;
+	// Use this for initialization
+	void Start () {		
+		open = false;
+	
+	}
+
+	void Update(){
+		if (Input.GetKey(KeyCode.I)) {
+			Panel.SetActive (true); 
+		} else {
+			Panel.SetActive (false); 
+		}
+	}
+			
+
+	// Update is called once per frame
+	/*void Update () {
+		if (Input.GetKey (KeyCode.I)) {
+			if (open == false) {
+				Panel.SetActive (true);
+				open = true;
+			}
+			else if (open==true) {
+				Panel.SetActive (false);
+				open = false;
+			}
+		}
+	
+	}*/
+}
